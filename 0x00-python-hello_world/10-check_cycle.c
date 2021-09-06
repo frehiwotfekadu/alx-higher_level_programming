14fc7bce6e80b544ff398f12a2efc2aaf3db1f32
Frehiwot Fekadu <fre.fekadu@gmail.com>
2021-09-06 17:00:27 +0000

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *check_cycle - checks if there a loop in a linked list
+ *@list: head of the linked least
+ *Return: 1 if there is a loop, 0 if there isn't
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *fast = NULL, *slow = NULL;
+if (!list)
+return (0);
+fast = list->next;
+slow = list;
+while (fast && fast->next)
+{
+if (fast == slow)
+return (1);
+fast = fast->next->next;
+slow = slow->next;
+}
+return (0);
+}
