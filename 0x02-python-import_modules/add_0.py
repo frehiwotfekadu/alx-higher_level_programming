a59b307d2dfc291e64795927fcd3eb775642d19b
Frehiwot Fekadu <fre.fekadu@gmail.com>
2021-09-09 11:27:04 +0000

    first commit

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first given integer
+        b: second given integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
