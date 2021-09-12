180423a5f1b2ba4f898a7f7409bf7b4daf816ebf
Frehiwot Fekadu <fre.fekadu@gmail.com>
2021-09-12 05:58:38 +0000

    first commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 # 7-add_tuple.py
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Adds 2 tuples."""
+    """Add two tuples."""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
