e6906ba8b324b682bc555598a49fa70de18d8116
frehiwotfekadu <fre.fekadu@gmail.com>
2021-09-15 08:57:18 -0700

    first commit

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 9-multiply_by_2.py
+
+
+def multiply_by_2(a_dictionary):
+    """Returns a new dictionary with all values multiplied by 2."""
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
