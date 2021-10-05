4b26bd8571d8049bc7981db8c6d022add10869ce
frehiwotfekadu <fre.fekadu@gmail.com>
2021-10-05 13:13:46 -0700

    Submit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define function for printing indented text
+"""
+
+
+def text_indentation(text):
+    """Prints indentations for a string
+    """
+    special = ['.', '?', ':']
+    if type(text) != str:
+        raise TypeError("text must be string")
+    for x in text:
+        print(x, end='')
+        if x in special:
+            print('\n\n', end='')
